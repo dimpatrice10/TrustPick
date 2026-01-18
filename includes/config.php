bdd43a1a41bca82a4201f665be1059f7da41958a
dimpatrice <dimpatrice3@gmail.com>
2026-01-18 11:59:49 +0100

    ok

--- includes/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Configuration de la base de données pour TrustPick
-return [
-    'db_host' => '127.0.0.1',
-    'db_name' => 'trustpick',
-    'db_user' => 'root',
-    'db_pass' => ''
-];

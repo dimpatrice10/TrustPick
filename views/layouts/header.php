cee16efdabcbfe19784be4133a4cc340fdd45816
dimpatrice <dimpatrice3@gmail.com>
2026-02-01 16:57:46 +0100

    feat(PWA): Implémenter un service worker dynamique et un manifest pour la compatibilité multi-environnements

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -38,8 +38,8 @@ $toasts = getToasts();
   <meta name="mobile-web-app-capable" content="yes">
   <meta name="application-name" content="TrustPick">
 
-  <!-- Manifest & Icons -->
-  <link rel="manifest" href="<?= url('manifest.json') ?>">
+  <!-- Manifest & Icons (dynamique pour compatibilité multi-environnements) -->
+  <link rel="manifest" href="<?= url('manifest.php') ?>">
   <link rel="icon" type="image/png" sizes="192x192" href="<?= url('assets/img/icon-192.png') ?>">
   <link rel="icon" type="image/png" sizes="512x512" href="<?= url('assets/img/icon-512.png') ?>">
 
